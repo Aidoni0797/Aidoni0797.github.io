5afd3f6470c856e54796703b1caa8179da57314b
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-22 22:15:04 +0500

    Update session.js

--- session.js
+++ session.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // Получаем данные пользователя (например, IP и геолокацию)
-    fetch('https://ipinfo.io/2.134.77.153?token=c0c78c76ac0ca3')  // Замените на ваш токен для получения IP
+    fetch('https://ipinfo.io/json?token=c0c78c76ac0ca3')  // Замените на ваш токен для получения IP
         .then(response => response.json())
         .then(data => {
             // Отправляем эти данные в бот или на сервер
