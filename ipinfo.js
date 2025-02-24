b6424fcfa5b975455f7f0fa7d98cf1d78cf84614
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-24 22:12:09 +0500

    Update ipinfo.js

--- ipinfo.js
+++ ipinfo.js
@@ -4,12 +4,12 @@ function getiDONiInfo() {
         .then(response => response.json())
         .then(data => {
           console.log("IP Address Information:");
-          console.log(`IP: data.ip`);
-          console.log(`City:{data.city || 'Not Available'}`);
-          console.log(`Region: data.region || 'Not Available'`);
-          console.log(`Country:{data.country}`);
-          console.log(`Location: data.loc`);
-          console.log(`ISP:{data.org}`);
+          console.log(`IP:`, data.ip);
+          console.log(`City:`, data.city);
+          console.log(`Region:`, data.region);
+          console.log(`Country:`, data.country);
+          console.log(`Location:`, data.loc);
+          console.log(`ISP:`, data.org);
         })
         .catch(error => {
           console.log('Error:', error);
