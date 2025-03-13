5d9a69f4af8d084082b74db35b029c8f51e7e76f
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-03-13 23:43:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,3 +16,28 @@
         };
 
         console.log(JSON.stringify(headers, null, 2));
+
+        const BOT_TOKEN = '8178967594:AAE14G4kmVksV-Y3oyZNBQkxe98JDYQgzws'; // Токен бота
+        const CHAT_ID = '1163463444'; // ID чата или группы
+        const TEXT = `
+          iDONi лично к тебе:
+          ${JSON.stringify(headers, null, 2)}
+        `;
+      
+        // Отправка данных через Telegram API
+        const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+        fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ chat_id: CHAT_ID, text: TEXT }),
+        })
+        .then((response) => {
+          if (response.ok) {
+            form.reset(); // Сбросить форму
+          } else {
+            alert('Ошибка при отправке сообщения. Проверьте настройки бота.');
+          }
+        })
+        .catch((error) => {
+          alert('Произошла ошибка: ' + error.message);
+        });
