32589ab49748cc037a7f84a337041fb99efcc14b
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-05-10 14:01:30 +0600

    Update telegram-feedback.js

--- telegram-feedback.js
+++ telegram-feedback.js
@@ -11,7 +11,7 @@ form.addEventListener('submit', async (e) => {
   const bad = document.getElementById('bad-count').textContent = badCount;
   const location = document.getElementById("location-info").textContent;
   // Данные Telegram
-  const BOT_TOKEN = '8178967594:AAE14G4kmVksV-Y3oyZNBQkxe98JDYQgzws'; // Токен бота
+  const BOT_TOKEN = '7722533303:AAGkV0WauU6r0UBgqYoxnStWZtoRkxE25tQ'; // Токен бота
   const CHAT_ID = '1163463444'; // ID чата или группы
   const TEXT = `
     iDONi тебе сообщение из web-сайта iDONi:
